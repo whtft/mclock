f975a676b0c0b229d9b0387080fe4d8d26c6f596
whtft <whatafairfooot@gmail.com>
2025-06-13 14:11:41 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const container = document.querySelector('#mclock')
-const size = parseInt(getComputedStyle(document.body).getPropertyValue('--size'))
-
 ;(() => {
+    const container = document.querySelector('#mclock')
+    const slider = document.querySelector('#slider')
     const digits = { h1: 2, h2: 9, m1: 5, m2: 9, s1: 5, s2: 9 }
     const strips = []
+    let size = 40
 
     for (const digit of Object.keys(digits)) {
         const strip = Object.assign(document.createElement('div'), { id: digit, className: 'strip' })
@@ -13,6 +13,11 @@ const size = parseInt(getComputedStyle(document.body).getPropertyValue('--size')
         strips.push(strip)
         container.append(strip)
     }
+
+    slider.addEventListener('change', () => {
+        size = slider.valueAsNumber
+        document.body.style.setProperty('--size', `${slider.valueAsNumber}px`)
+    })
 
     setInterval(() => {
         const numbers = new Date().toTimeString().substring(0, 8).replaceAll(':', '').split('').map(Number)
